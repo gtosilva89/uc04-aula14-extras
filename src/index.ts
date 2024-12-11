d3de74495ba300ce0f1ccc2239f1ca393518c68a
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-12-10 21:28:15 -0300

    LEITURA DE ARQUIVOS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Olá mundo");
+// bibliotecas e linguagem
+// ler um arquivo de texto linha a linha
+
+// fs - files system
+import fs from "node:fs/promises";
+
+async function main() {
+  let lines: string[] = [];
+
+  const file = await fs.open("exemplo.txt");
+
+  for await (const line of file.readLines()){
+    console.log(line);
+    lines.push(line)
+  }
+
+  console.log(lines);
+}
+
+(async () => {
+  await main();
+})();
